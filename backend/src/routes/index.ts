6f6034b7716e835fc0d82a760486f74bad421542
fernandomarca <fernandomarca@hotmail.com>
2020-11-02 22:45:32 -0300

    test de debug

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,16 @@
 import { Router } from 'express';
 
-const routes = Router()
+const routes = Router();
 
-export default routes
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email
+  }
+
+  return response.json(user)
+})
+
+export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,11 @@ import routes from './routes'
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'hello world'
-  })
-})
+app.use(express.json());
+
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('server started on port 3333');
 })
+
