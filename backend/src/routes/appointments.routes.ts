8ab80ba7a61834f4a52f72047e64ef69a06b50ae
fernandomarca <fernandomarca@hotmail.com>
2020-11-03 11:57:41 -0300

    model-repository

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+
+import AppointmentRepository from '../repositories/AppointmentRepository';
+
+const appointmentsRouter = Router();
+
+const appointmentRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+
+  const { provider, date } = request.body;
+
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentRepository.findByDate(parseDate);
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({
+      message: 'This appointment is already booked'
+    })
+  }
+
+  const appointment = appointmentRepository.create(provider, parseDate);
+
+  return response.json(appointment)
+})
+
+export default appointmentsRouter
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,10 @@ import routes from './routes'
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(routes)
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('server started on port 3333');
 })
-
