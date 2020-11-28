692c0fbe3e2d09d434e9b86f53bea7f0ad538931
fernandomarca <fernandomarca@hotmail.com>
2020-11-28 17:10:19 -0300

    bug corrected

--- backend/src/shared/infra/typeorm/migrations/1604511319207-CreateUsers.ts
+++ backend/src/shared/infra/typeorm/migrations/1604511319207-CreateUsers.ts
@@ -4,45 +4,45 @@ export default class CreateUsers1604511319207 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(new Table({
-      name: 'users',
+      name: "users",
       columns: [
         {
-          name: 'id',
-          type: 'uuid',
+          name: "id",
+          type: "uuid",
           isPrimary: true,
-          generationStrategy: 'uuid',
-          default: 'uuid_generate_v4()'
+          generationStrategy: "uuid",
+          default: "uuid_generate_v4()"
         },
         {
-          name: 'name',
-          type: 'varchar',
+          name: "name",
+          type: "varchar",
           isNullable: false,
         },
         {
-          name: 'email',
-          type: 'varchar',
+          name: "email",
+          type: "varchar",
           isUnique: true
         },
         {
-          name: 'password',
-          type: 'varchar'
+          name: "password",
+          type: "varchar"
         },
         {
-          name: 'created_at',
-          type: 'timestamp with time zone',
+          name: "created_at",
+          type: "timestamp with time zone",
           default: 'now()',
         },
         {
-          name: 'updated_at',
-          type: 'timestamp with time zone',
-          default: 'now()',
+          name: "updated_at",
+          type: "timestamp with time zone",
+          default: "now()",
         }
       ],
     }))
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('users');
+    await queryRunner.dropTable("users");
   }
 
 }
