f9009ccab966ac4d98fe87b9341a9843704bf11f
fernandomarca <fernandomarca@hotmail.com>
2020-11-30 19:15:41 -0300

    excluindo horas antigas

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -19,12 +19,16 @@ describe('List Provider Month Availability', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0)
+      date: new Date(2020, 4, 20, 14, 0, 0)
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0)
+      date: new Date(2020, 4, 20, 15, 0, 0)
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailabilityService.execute({
@@ -36,9 +40,12 @@ describe('List Provider Month Availability', () => {
 
     expect(availability).toEqual(expect.arrayContaining([
       { hour: 8, availability: false },
-      { hour: 9, availability: true },
+      { hour: 9, availability: false },
       { hour: 10, availability: false },
-      { hour: 11, availability: true },
+      { hour: 13, availability: true },
+      { hour: 14, availability: false },
+      { hour: 15, availability: false },
+      { hour: 16, availability: true },
     ]));
   });
 
