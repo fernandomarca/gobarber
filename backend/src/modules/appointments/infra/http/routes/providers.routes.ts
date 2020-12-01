32f8dc42ed4756dd27e63326bbd21c125329e487
fernandomarca <fernandomarca@hotmail.com>
2020-11-30 23:42:28 -0300

    appointments finish

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -4,13 +4,25 @@ import ensuraAuthenticated from '@modules/users/infra/http/middlewares/ensuraAut
 
 import ProvidersController from '../controllers/ProvidersController';
 
+import ProviderDayAvaiabilityController from '../controllers/ProviderDayAvaiabilityController';
+
+import ProviderMonthAvaiabilityController from '../controllers/ProviderMonthAvaiabilityController';
+
 const providersRouter = Router();
 
 const providersController = new ProvidersController();
 
+const providerDayAvaiabilityController = new ProviderDayAvaiabilityController();
+
+const providerMonthAvaiabilityController = new ProviderMonthAvaiabilityController();
+
 providersRouter.use(ensuraAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get('/:provider_id/month-availability', providerMonthAvaiabilityController.index);
+
+providersRouter.get('/:provider_id/day-availability', providerDayAvaiabilityController.index);
 
 export default providersRouter;
 
