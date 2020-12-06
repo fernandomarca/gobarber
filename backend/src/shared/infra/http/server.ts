142457d1a8347c425d3180b9f0b5de39aab808ed
fernandomarca <fernandomarca@hotmail.com>
2020-12-05 21:56:10 -0300

    rate limit

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -11,8 +11,11 @@ import uploadConfig from '@config/upload';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
+import rateLimiter from '@shared/middleware/rateLimiter';
 
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(cors());
 
